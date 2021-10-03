a77583044cde0de44776df6cc966b9d1b048838c
Borja González <borja@bgon.es>
2021-10-03 20:04:47 +0200

    remove console.log

--- src/Hide.tsx
+++ src/Hide.tsx
@@ -2,7 +2,6 @@ import { useModalContext } from "./ModalContext";
 
 export const Hide = () => {
   const { hideModal } = useModalContext();
-  console.log("render0");
 
   return <button onClick={hideModal}>HIDE MODALS</button>;
 };
