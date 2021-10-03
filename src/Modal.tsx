5b9864c26eefed3905ae408087205a7532e01828
Borja González <borja@bgon.es>
2021-10-03 20:08:19 +0200

    use hideModal from inside Modal

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,3 +1,12 @@
 import { FC } from "react";
+import { useModalContext } from "./ModalContext";
 
-export const Modal: FC = ({ children }) => <h2>{children}</h2>;
+export const Modal: FC = ({ children }) => {
+  const { hideModal } = useModalContext();
+  return (
+    <h2>
+      {children}
+      <button onClick={hideModal}>X</button>
+    </h2>
+  );
+};
